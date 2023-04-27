7a11131268d32bcfcd09a98480a3595eb4891e02
Bahri AKÇAN <akcan750bahri09@gmail.com>
2023-04-27 13:20:10 +0300

    Giriş Formu Firebaseye bağlandı ve giriş başarılı oldugunda Genel forma geçiş yapılacak.

--- GirisForm.cs
+++ GirisForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Firebase.Auth.Providers;
+using Firebase.Auth;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,9 +19,53 @@ namespace Petrol_ve_Marketcilik_Otomasyonu
             InitializeComponent();
         }
 
-        private void LoginBtn_Click(object sender, EventArgs e)
+        private async void LoginBtn_Click(object sender, EventArgs e)
         {
+            if (!internetKontrol.internet())
+            {
+                MessageBox.Show("Internet Baglantısı Yok.");
+                return;
+            };
+            bool login = await loginAsync();
+            if (login)
+            {
+                GenelForm genelForm = new GenelForm();
+                genelForm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Giris Basarisiz");
+            }
+        }
+
+        private async Task<bool> loginAsync()
+        {
+            object userCredential;
+            var config = new FirebaseAuthConfig
+            {
+                ApiKey = "AIzaSyBPP5gvm8JvBT43dAsqUYlOXxy_GUcMyAU",
+                AuthDomain = "petrol-ve-marketcilik.firebaseapp.com",
+                Providers = new FirebaseAuthProvider[]
+                {
+                    new GoogleProvider().AddScopes("email"),
+                    new EmailProvider()
+
+                },
+            };
+
+            var client = new FirebaseAuthClient(config);
+            try
+            {
+                userCredential = await client.SignInWithEmailAndPasswordAsync(usernameTxt.Text, passwordTxt.Text);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return userCredential != null;
 
         }
+
     }
 }
