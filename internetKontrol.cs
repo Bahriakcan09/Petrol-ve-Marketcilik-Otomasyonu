7b46dfb8f2b1a59fa7884edfc6046f9b68e294a3
Bahri AKÇAN <akcan750bahri09@gmail.com>
2023-04-27 13:01:04 +0300

    İnternet kontrolü için sınıf eklendi.

---
+++ internetKontrol.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petrol_ve_Marketcilik_Otomasyonu
+{
+    public class internetKontrol
+    {
+        public static bool internet()
+        {
+            WebClient webClient = new WebClient();
+            try
+            {
+                webClient.OpenRead("https://google.com");
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
